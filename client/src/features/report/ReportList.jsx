a8dd5cd1c745dbb0de0e74da284ae934617528fc
Aidan <aidan.s.ranney@gmail.com>
2019-06-10 15:40:47 -0700

    new reportdashboard

--- client/src/features/report/ReportList.jsx
+++ client/src/features/report/ReportList.jsx
@@ -1,125 +1,177 @@
-import React, { Component } from 'react';
-import ReactTable from 'react-table';
-import 'react-table/react-table.css';
-import DatePicker from 'react-datepicker'
-import "react-datepicker/dist/react-datepicker.css"
-import * as ui from '../record/ui';
+import React, { Component } from "react";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-function handleHTTPErrors(response) {
-if (!response.ok) {
-throw Error(response.statusText);
-}
-return response;
-}
-
-async function fetchData(endpoint, page, pageSize, sorted, filtered, handleData) {
-    let requestBody = {
-    page: page,
-    pageSize: pageSize,
-    sorted: sorted,
-    filtered: filtered,
+class RecordList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      page: 0,
+      pages: 0,
+      pageSize: 20,
+      sorted: [],
+      loading: false,
+      startDate: new Date(),
+      endDate: new Date()
     };
-    try {
-        let result = await fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-            body: JSON.stringify(requestBody)
-        });
-        result = await handleHTTPErrors(result);
-        const records = await result.json();
-        return handleData(records);
-    } catch (err) {
-        console.log("fetchData failed: " + err);
-        return err;
-    }
-}
-
-class ReportList extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            startDate: new Date(),
-            endDate: new Date(),
-            data: [],
-            pages: 0,
-            loading: false
-        };
     this.handleChangeStart = this.handleChangeStart.bind(this);
     this.handleChangeEnd = this.handleChangeEnd.bind(this);
+  }
 
-    }
-
-    handleChangeStart(date) {
+  handleChangeStart(date) {
     this.setState({
-    startDate: date
+      startDate: date
     });
-    }
+  }
 
-    handleChangeEnd(date) {
+  handleChangeEnd(date) {
     this.setState({
-    endDate: date
+      endDate: date
     });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.props.type !== prevProps.type ||
+      this.props.filter !== prevProps.filter
+    ) {
+      switch (this.props.type) {
+        case "keys":
+          this.setState({
+            loading: true
+          });
+          fetchRecordData(
+            this.props.type,
+            this.state.page,
+            this.state.pageSize,
+            this.state.sorted,
+            this.props.filter,
+            res => {
+              this.setState({
+                data: res.data,
+                page: 0,
+                pages: res.pages,
+                loading: false
+              });
+            }
+          );
+          break;
+        case "properties":
+          this.setState({
+            loading: true
+          });
+          fetchRecordData(
+            this.props.type,
+            this.state.page,
+            this.state.pageSize,
+            this.state.sorted,
+            this.props.filter,
+            res => {
+              this.setState({
+                data: res.data,
+                page: 0,
+                pages: res.pages,
+                loading: false
+              });
+            }
+          );
+          break;
+        case "people":
+          this.setState({
+            loading: true
+          });
+          fetchRecordData(
+            this.props.type,
+            this.state.page,
+            this.state.pageSize,
+            this.state.sorted,
+            this.props.filter,
+            res => {
+              this.setState({
+                data: res.data,
+                page: 0,
+                pages: res.pages,
+                loading: false
+              });
+            }
+          );
+          break;
+        default:
+          return;
+      }
     }
+  }
 
-    render() {
-
-        return (
-            <div>
-                <container>
-                    <DatePicker
-                    selected={this.state.startDate}
-                    selectsStart
-                    startDate={this.state.startDate}
-                    endDate={this.state.endDate}
-                    onChange={this.handleChangeStart}
-                    showYearDropdown
-                    showMonthDropdown
-                    withPortal
-                    />
-                    <DatePicker
-                    selected={this.state.endDate}
-                    selectsEnd
-                    startDate={this.state.startDate}
-                    endDate={this.state.endDate}
-                    onChange={this.handleChangeEnd}
-                    minDate={this.state.startDate}
-                    showYearDropdown
-                    showMonthDropdown
-                    withPortal
-                    />
-                    <ReactTable
-                    className = '-highlight'
-                    data={this.state.data}
-                    pages={this.state.pages}
-                    columns={ui.peopleColumns}
-                    minRows={1}
-                    defaultPageSize={20}
-                    loading={this.state.loading}
-                    showPagination={true}
-                    showPaginationTop={false}
-                    showPaginationBottom={true}
-                    pageSizeOptions={[5, 10, 20, 25, 50, 100]}
-                    manual
-                    onFetchData={(state, instance) => {
-                        this.setState({loading: true});
-                        fetchData("keys", state.page, state.pageSize, state.sorted, state.filtered, (res) => {
-                            this.setState({
-                            data: res.data,
-                            pages: res.pages,
-                            loading: false
-                            });
-                        });
-                    }}
-                    />  
-                </container>
-            </div>
-        )
-
-    }
+  render() {
+    return (
+      <div>
+        <DatePicker
+          selected={this.state.startDate}
+          selectsStart
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleChangeStart}
+          showYearDropdown
+          showMonthDropdown
+          withPortal
+        />
+        <DatePicker
+          selected={this.state.endDate}
+          selectsEnd
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleChangeEnd}
+          minDate={this.state.startDate}
+          showYearDropdown
+          showMonthDropdown
+          withPortal
+        />
+        <ReactTable
+          className="-highlight"
+          data={this.state.data}
+          page={this.state.page}
+          pages={this.state.pages}
+          columns={this.props.columns}
+          minRows={1}
+          defaultPageSize={20}
+          loading={this.state.loading}
+          showPagination={true}
+          showPaginationTop={false}
+          showPaginationBottom={true}
+          pageSizeOptions={[5, 10, 20, 25, 50, 100]}
+          manual
+          onPageChange={pageIndex => {
+            this.setState({ page: pageIndex });
+          }}
+          onPageSizeChange={(pageSize, pageIndex) => {
+            this.setState({ page: pageIndex, pageSize: pageSize });
+          }}
+          onFetchData={(state, instance) => {
+            this.setState({ loading: true });
+            fetchRecordData(
+              this.props.type,
+              state.page,
+              state.pageSize,
+              state.sorted,
+              this.props.filter,
+              res => {
+                this.setState({
+                  data: res.data,
+                  page: state.page,
+                  pages: res.pages,
+                  loading: false
+                });
+              }
+            );
+          }}
+        />
+      </div>
+      
+    );
+  }
 }
-
 
 export default ReportList;
