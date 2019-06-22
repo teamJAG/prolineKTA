5880f4bf75e6fcd3e5ec4b93f35ca8ac07440a43
Aidan <aidan.s.ranney@gmail.com>
2019-06-21 18:29:58 -0700

    credits

--- server/api/controllers/propertyApi.js
+++ server/api/controllers/propertyApi.js
@@ -1,64 +1,5 @@
 const db = require("../db/connection");
 
-// //Get a property record
-// async function getPropertyStatus(req, res) {
-//   const { propertyId } = req.body;
-//   let recordQuery = queries.propertyRecord;
-//   recordQuery += `property_number LIKE ${propertyNumber} AND
-//     property_name LIKE '${propertyName}' AND address LIKE
-//     '${address}' AND city LIKE ${city} `;
-//   console.log(recordQuery);
-//   try {
-//     const rows = await db.dbQuery(recordQuery);
-//     let propertyRecord = rows[0];
-//     if (rows.length > 1) {
-//       throw new Error("More than one property matched in database.");
-//     } else if (rows.length < 1) {
-//       throw new Error("No records matched in database.");
-//     }
-//     let payload = {
-//       key: {
-//         keyId: propertyRecord.key_id,
-//         address: propertyRecord.address,
-//         city: propertyRecord.city,
-//         propertyName: propertyRecord.property_name,
-//         propertyNumber: propertyRecord.property_number,
-//         propertyType: propertyRecord.property_type,
-//         keyType: propertyRecord.key_type,
-//         keyStatus: propertyRecord.key_status
-//       }
-//     };
-//     if (rows[0].key_status === 0) {
-//       const transactionQuery =
-//         queries.transactionQuery +
-//         `WHERE key_tab_key_id LIKE ${propertyRecord.key_id} 
-//       ORDER BY checked_out DESC LIMIT 1`;
-//       let transaction = await db.dbQuery(transactionQuery);
-//       transaction = transaction[0];
-//       payload = Object.assign(payload, {
-//         trans: {
-//           id: transaction.trans_id,
-//           firstName: transaction.first_name,
-//           lastName: transaction.last_name,
-//           company: transaction.company,
-//           checkedOut: transaction.checked_out.toLocaleString(),
-//           dueDate: transaction.due_date.toLocaleString(),
-//           deposit: transaction.deposit,
-//           depositType: transaction.deposit_type,
-//           fees: transaction.fees,
-//           notes: transaction.notes
-//         }
-//       });
-//     }
-//     console.log(payload);
-//     res.status(200).json(payload);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(404).json(err);
-//     return;
-//   }
-// }
-
 //Create new property
 async function createProperty(req, res) {
   const {
@@ -71,14 +12,14 @@ async function createProperty(req, res) {
     comments
   } = req.body;
 
-  const cityString = `INSERT IGNORE INTO proline.city_tab (city) VALUES ("${city}") `
+  const cityString = `INSERT IGNORE INTO proline.city_tab (city) VALUES ("${city.toUpperCase()}") `
 
   const propertyString = `INSERT IGNORE INTO proline.property_tab (property_number, property_name, property_type, comments) VALUES 
-  ("${propertyNumber}", "${propertyName}", "${propertyType}", "${comments}")`;
+  ("${propertyNumber.toUpperCase()}", "${propertyName.toUpperCase()}", "${propertyType.toUpperCase()}", "${comments.toUpperCase()}")`;
 
   const addressString = `INSERT INTO proline.address_tab (address, postal_code, city_tab_city_id, property_tab_property_id) VALUES 
-    ("${address}", "${postalCode}", (SELECT city_id FROM proline.city_tab WHERE city LIKE "${city}"), 
-    (SELECT property_id FROM proline.property_tab WHERE property_number LIKE "${propertyNumber}") )`;
+    ("${address.toUpperCase()}", "${postalCode.toUpperCase()}", (SELECT city_id FROM proline.city_tab WHERE city LIKE "${city.toUpperCase()}"), 
+    (SELECT property_id FROM proline.property_tab WHERE property_number LIKE "${propertyNumber.toUpperCase()}") )`;
 
   console.log(cityString);
   console.log(propertyString);
@@ -115,14 +56,14 @@ async function updateProperty(req, res) {
     comments
   } = req.body;
 
-  const cityString = `INSERT IGNORE INTO proline.city_tab (city) VALUES ("${city}") `
+  const cityString = `INSERT IGNORE INTO proline.city_tab (city) VALUES ("${city.toUpperCase()}") `
 
-  const updatePropertyString = `UPDATE proline.property_tab SET property_number = "${propertyNumber}",  
-    property_name = "${propertyName}", property_type = "${propertyType}", comments = "${comments}"
+  const updatePropertyString = `UPDATE proline.property_tab SET property_number = "${propertyNumber.toUpperCase()}",  
+    property_name = "${propertyName.toUpperCase()}", property_type = "${propertyType.toUpperCase()}", comments = "${comments.toUpperCase()}"
     WHERE property_id = ${propertyId}`
 
-  const updateAddressString = `UPDATE proline.address_tab SET address = "${address}", postal_code = "${postalCode}", 
-    city_tab_city_id = (SELECT city_id FROM proline.city_tab WHERE city = "${city}"), 
+  const updateAddressString = `UPDATE proline.address_tab SET address = "${address.toUpperCase()}", postal_code = "${postalCode.toUpperCase()}", 
+    city_tab_city_id = (SELECT city_id FROM proline.city_tab WHERE city = "${city.toUpperCase()}"), 
     property_tab_property_id = "${propertyId}" WHERE address_id = ${addressId} `
 
   console.log(cityString);
